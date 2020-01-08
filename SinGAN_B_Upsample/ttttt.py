48848edac531018a2125c03419fc17811d876d79
HH <hhcs9527@gmail.com>
2020-01-08 14:50:28 +0800

    remove old codes

--- SinGAN_B_Upsample/ttttt.py
+++
@@ -1,5 +0,0 @@
-import torch
-import numpy as np
-
-G = torch.load('./TrainedModels/cityscapes/scale_factor=0.750000_seg/0/netG.pth')
-print(np.abs(G['head.conv.weight'].cpu().numpy()).mean(axis=(0,2,3)))
